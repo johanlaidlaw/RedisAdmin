a26e6cb3863d57f897d2f697c692fb2ec0c2dcd7
Roy Fox <roysfox@gmail.com>
2012-01-20 10:38:28 +0000

    Bugfix: don't try to display stats when Redis server is not running

--- view/home/index.php
+++ view/home/index.php
@@ -13,24 +13,27 @@
 </div>
 
 <div id="right_container">
-	<div id="server_stats">
-		<h2>Server stats</h2>
 		<table>
             <?php
-        try {
-            $redis = lib\redis\RedisClient::getPredisObject();
-         } catch (\Predis\Network\ConnectionException $e) {
+                try {
+                    $redis = lib\redis\RedisClient::getPredisObject();
+                ?>
+                <div id="server_stats">
+                    <h2>Server stats</h2>
+                 <?php
+                    foreach($redis->info() as $desc => $info){
+                        if($desc == 'db0'){
+                            echo '<tr><td style="background-color:#E3E3E3" colspan="2">'.$desc.'</td></tr>';
+                            foreach($info as $d => $d_val)
+                                echo '<tr><td>'.$d.'</td><td>'.$d_val.'</td></tr>';
+                        }else
+                            echo '<tr><td>'.$desc.'</td><td>'.$info.'</td></tr>';
+          		    }
+                    echo "</div>";
+                 } catch (\Predis\Network\ConnectionException $e) {
 
-         }
-		foreach($redis->info() as $desc => $info){
-			if($desc == 'db0'){
-				echo '<tr><td style="background-color:#E3E3E3" colspan="2">'.$desc.'</td></tr>';
-				foreach($info as $d => $d_val)
-					echo '<tr><td>'.$d.'</td><td>'.$d_val.'</td></tr>';
-			}else
-				echo '<tr><td>'.$desc.'</td><td>'.$info.'</td></tr>';
-		}
+                 }
 		?>
 		</table>
-	</div>
+
 </div>
