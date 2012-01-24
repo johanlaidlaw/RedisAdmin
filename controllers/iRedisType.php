2c477eac3e8fdb891f1575e222a6af60805075b4
Johan Laidlaw <johanlaidlaw@gmail.com>
2012-01-24 15:16:51 +0000

    Renamed file

---
+++ controllers/iRedisType.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * User: johanlaidlaw
+ * Date: 1/19/12
+ * Time: 7:13 PM
+ */
+ 
+interface iRedisType{
+
+    public function editField();
+    public function getValues();
+    
+
+}
