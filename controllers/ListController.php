39cc1373239054f63bcfe9e47e86fe356fdbe051
Angel Brasero <angel.brasero@gmail.com>
2012-03-07 14:07:16 +0000

    List support

---
+++ controllers/ListController.php
@@ -0,0 +1,34 @@
+<?php
+
+class ListController extends RedisController implements iRedisType
+{
+
+
+    public function addField(){
+        $this->redis->rpush($this->params['key'], $this->params['value']);
+        $this->getAllFields($this->params['key']);
+    }
+
+    public function editField(){
+        $bool = $this->redis->lset($this->params['key'], $this->params['field'], $this->params['value']);
+        echo json_encode($bool);
+
+    }
+    public function getValues(){
+        $this->getAllFields($this->params['key']);
+    }
+
+
+
+
+
+
+
+
+
+    private function getAllFields($key){
+    	$size = $this->redis->llen($key);
+        $fields = $this->redis->lrange($key, 0, $size-1);
+        $this->returnAllFields($fields, 'list');
+    }
+}
