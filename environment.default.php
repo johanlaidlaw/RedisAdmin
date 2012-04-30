bf89877bb284912c94f06b2ef9e16a01641a9e0a
Johan Laidlaw <johanlaidlaw@gmail.com>
2012-04-30 14:02:50 +0100

    Print correct path when no environment file is found

--- environment.default.php
+++ environment.default.php
@@ -14,11 +14,11 @@ define("REDIS_DB_PASSWORD", "mypassword");
 
 // First read the settings for this environment!
 if(is_readable(dirname(__FILE__).'/environment.php')) {
-	include(dirname(__FILE__).'/environment.php');
+	include(dirname(__FILE__) . '/environment.php');
 } else {
 	echo '<h2>Ooops..</h2>';
-	echo 'You should create your own environment.php in "'.($_SERVER['DOCUMENT_ROOT'].'environment.php').'" with your own environment-specific constants!<br />';
-	echo 'See "'.$_SERVER['DOCUMENT_ROOT'].'environment.default.php'.'" for more info.<br />';
+	echo 'You should create your own environment.php in "'.($_SERVER['DOCUMENT_ROOT'].'/environment.php').'" with your own environment-specific constants!<br />';
+	echo 'See "'.$_SERVER['DOCUMENT_ROOT'].'/environment.default.php'.'" for more info.<br />';
 	exit;
 }
 
